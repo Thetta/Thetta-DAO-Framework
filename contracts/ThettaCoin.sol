cfa75c6cf6ae670bef26ed544f803c7468f56130
Anthony Akentiev <anthony.akentiev@gmail.com>
2017-11-08 19:59:18 +0300

    Remove unneeded tests

--- contracts/ThettaCoin.sol
+++ contracts/ThettaCoin.sol
@@ -1,16 +1,17 @@
 pragma solidity ^0.4.4;
+//pragma solidity ^0.4.15;
 
 // This is just a simple example of a coin-like contract.
 // It is not standards compatible and cannot be expected to talk to other
 // coin/token contracts. If you want to create a standards-compliant
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
-contract MetaCoin {
+contract ThettaCoin {
 	mapping (address => uint) balances;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	function MetaCoin() {
+	function ThettaCoin() {
 		balances[tx.origin] = 10000;
 	}
 
