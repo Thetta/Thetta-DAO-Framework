1ec78943295a5a9fe63af4702360773b090502e3
Anthony Akentiev <anthony.akentiev@gmail.com>
2018-05-12 17:03:33 +0300

    Adding moneyflow test stubs

--- contracts/moneyflow/IMoneyflow.sol
+++ contracts/moneyflow/IMoneyflow.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.4.15;
 contract IMoneyflow {
 	// send revenue here!
 	function getRevenueEndpointAddress()public constant returns(address);
-
 	function getDonationEndpointAddress()public constant returns(address);
+
 	// send all donations to the msg.sender (onlyOwner of this contract)
+	// TODO: should require VOTING
 	function withdrawDonations()public;
 
 // Receivers
--- test/moneyflow_tests.js
+++ test/moneyflow_tests.js
@@ -26,10 +26,6 @@ global.contract('Moneyflow', (accounts) => {
 		moneyflowInstance = await MoneyFlow.new({from: creator});
 	});
 
-	global.it('should allow to get donations',async() => {
-		// TODO: write test
-	});
-
 	global.it('should allow to send revenue',async() => {
 		// Moneyflow.getRevenueEndpointAddress() -> FallbackToWeiReceiver -> Fund
 		const revEndpoint = await moneyflowInstance.getRevenueEndpointAddress();
@@ -50,6 +46,72 @@ global.contract('Moneyflow', (accounts) => {
 		// money should end up in the fund
 		const balance = await web3.eth.getBalance(fund.address);
 		global.assert.equal(balance,1000000000000000,'Money should be transferred to the fund');
+
+		// TODO: test fund.flush()
+	});
+
+	global.it('should allow to get donations',async() => {
+		// TODO: write test
+		// use getDonationEndpointAddress() to get address 
+		//
+		// send some money to the donation endpoint 
+		//
+		// withdraw that money by the creator (without voting!!!)
+	});
+
+	global.it('should process money with WeiTopDownSplitter + 3 WeiAbsoluteExpense',async() => {
+		// TODO:
+		// create WeiTopDownSplitter 
+		
+		// add 3 WeiAbsoluteExpense outputs to the splitter
+		
+		// add WeiTopDownSplitter to the moneyflow
+		// await moneyflowInstance.setWeiReceiver(splitter.address);
+
+		// now send some money to the revenue endpoint 
+		
+		// money should end up in the outputs
+	});
+
+	global.it('should process money with WeiUnsortedSplitter + 3 WeiAbsoluteExpense',async() => {
+		// TODO:
+		// create WeiUnsortedSplitter 
+		
+		// add 3 WeiAbsoluteExpense outputs to the splitter
+		
+		// add WeiTopDownSplitter to the moneyflow
+		// await moneyflowInstance.setWeiReceiver(splitter.address);
+
+		// now send some money to the revenue endpoint 
+		
+		// money should end up in the outputs
+	});
+
+	global.it('should process money with a scheme just like in the paper',async() => {
+		// TODO:
+		// Document is here:
+		// https://docs.google.com/document/d/15UOnXM_iPudD95m-UYBcYns-SeqM2ksDecjYhZrqybQ/edit?usp=sharing
+		//
+		// top-down splitter 
+		//		Spends - unsorted splitter
+		//			Salaries - unsorted splitter 
+		//				Employee1 
+		//				Employee2 
+		//				Employee3 
+		//			Other - unsorted splitter 
+		//				Office 
+		//				Internet
+		//			Tasks - unsorted splitter
+		//				Task1
+		//				Task2
+		//				Task3
+		//		Bonuses - unsorted splitter
+		//			Employee1 
+		//			Employee2 
+		//			Employee3 
+		//		Rest - unsorted splitter
+		//			ReserveFund - fund 
+		//			DividendsFund - fund
 	});
 });
 
