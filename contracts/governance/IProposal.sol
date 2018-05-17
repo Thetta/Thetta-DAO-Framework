b61e50276ff1ed2ba4f393689a0b1fc247842ad5
Anthony Akentiev <anthony.akentiev@gmail.com>
2018-05-17 11:36:44 +0300

    Cont

--- contracts/governance/IProposal.sol
+++ contracts/governance/IProposal.sol
@@ -6,5 +6,8 @@ import '../IMicrocompany.sol';
 interface IProposal {
 	function action(IMicrocompanyBase _mc, IVoting _voting)public;
 	function getVoting()public constant returns(address vote);
+
+	// ???
+	// function isOpen() public constant returns(bool);
 }
 
