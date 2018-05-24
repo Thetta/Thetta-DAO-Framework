2b75149e56eef20151ab1ab8200ead02a4ddacef
Anthony Akentiev <anthony.akentiev@gmail.com>
2018-05-24 14:00:39 +0300

    Add erc20 file stubs

---
+++ contracts/moneyflow/erc20/Erc20Fund.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.15;
+
+import "../IMoneyflow.sol";
+
+import "./WeiExpense.sol";
+
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
