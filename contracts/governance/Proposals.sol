f6815ab99ec4b2632075ffebc8ea5fed7b601ab6
KIrill Guzenko <kirill@chain.cloud>
2018-05-29 14:51:50 +0300

    #52: InformalProposal

---
+++ contracts/governance/Proposals.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.15;
+
+import "../IDaoBase.sol";
+
+import "../governance/Voting.sol";
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract InformalProposal is IProposal, Ownable {
+	string proposalText = '';
+
+	function InformalProposal(string _proposalText) public {
+		proposalText = _proposalText;
+	}
+
+	function getProposalText()public constant returns(string){
+		return proposalText;
+	}
+
+// IVoting implementation
+
+	function setVoting(IVoting _voting) public onlyOwner{
+		voting = _voting;
+	}
+
+	function getVoting()public constant returns(IVoting){
+		return voting;
+	}
+}
