9bf364e3b6de9e76ca41f0f7da43ed547e89435c
Anthony Akentiev <anthony.akentiev@gmail.com>
2018-05-30 16:01:27 +0300

    Trying to fix...

--- contracts/dao_examples/OutOfGasTest.sol
+++ contracts/dao_examples/OutOfGasTest.sol
@@ -8,8 +8,116 @@ import '../tokens/StdDaoToken.sol';
 //import "../utils/GenericCaller.sol";
 
 // for GenericCallerTEST
-import "../governance/Voting.sol";
+//import "../governance/Voting.sol";
 import "../governance/IProposal.sol";
+
+contract Voting_1p1vTEST is IVoting, Ownable {
+	IDaoBase mc;
+	IProposal proposal; 
+	bool isCalled = false;
+	uint public minutesToVote;
+////////
+	bytes32 groupHash;
+
+	mapping (uint=>address) employeesVoted;
+	uint employeesVotedCount = 0;
+	mapping (address=>bool) votes;
+
+////////
+	// we can use _origin instead of tx.origin
+	function Voting_1p1vTEST(IDaoBase _mc, IProposal _proposal, 
+								address _origin, 
+								uint _minutesToVote, bytes32 _groupHash, bytes32 _emptyParam){
+		mc = _mc;
+		minutesToVote = _minutesToVote;
+		groupHash = _groupHash;
+
+		// the caller must be a member of the group!
+		require(mc.isGroupMemberByHash(groupHash,_origin));
+
+		internalVote(_origin, true);
+	}
+////////////////
+	function callActionIfEnded() public {
+		if(!isCalled && isFinished() && isYes()){
+			// should not be callable again!!!
+			isCalled = true;
+
+			// can throw!
+			proposal.action(mc, this);
+		}
+	}
+
+	function isYes()public constant returns(bool){
+		// WARNING: this line is commented, so will not check if voting is finished!
+		//if(!isFinished(){return false;}
+		var(yesResults, noResults, totalResults) = getFinalResults();
+
+		// TODO: calculate results
+		// TODO: JUST FOR DEBUGGGGG!!!
+		return (yesResults > totalResults/2) && (totalResults>1);
+	}
+
+	// TODO: out of GAS!!!
+	function isFinished() public constant returns(bool){
+		// 1 - if minutes elapsed
+
+		// 2 - if voted enough participants
+		//if((mc.getEmployeesCount()/2) < employeesVotedCount){
+	   //		return true;
+		//}
+
+		// TODO: JUST FOR DEBUGGGGG!!!
+		var(yesResults, noResults, totalResults) = getFinalResults();
+		return (totalResults>1);
+	}
+	
+////////////////
+	function vote(bool _yes, uint _tokenAmount) public {
+		require(!isFinished());
+
+		require(mc.isGroupMemberByHash(groupHash,msg.sender));
+
+		internalVote(msg.sender, _yes);
+	}
+
+	function internalVote(address _who, bool _yes) internal {
+		employeesVoted[employeesVotedCount] = _who;
+		employeesVotedCount++;
+
+		votes[_who] = _yes;
+
+		callActionIfEnded();
+	}
+
+	function cancelVoting() public onlyOwner {
+		// TODO:
+	}
+
+	function getFinalResults() public constant returns(uint yesResults, uint noResults, uint totalResults){
+		yesResults = 0;
+		noResults = 0;
+		totalResults = 0;
+
+		// employees could be fired or added IN THE MIDDLE of the voting 
+		//
+		// so here we should iterate again over all microcompany employees and check if they voted yes or no 
+		// each employee has 1 vote 
+		for(uint i=0; i<employeesVotedCount; ++i){
+			address e = employeesVoted[i];
+
+			if(mc.isGroupMemberByHash(groupHash,e)){
+				// count this vote
+				if(votes[e]){
+					yesResults++;
+				}else{
+					noResults++;
+				}
+				totalResults++;
+			}
+		}
+	}
+}
 
 contract GenericProposalTEST is IProposal, Ownable {
 	IVoting voting;
@@ -82,37 +190,46 @@ contract GenericCallerTEST is DaoClient, Ownable {
 	function doAction(string _permissionId, address _target, address _origin, string _methodSig, bytes32[] _params) public returns(address proposalOut) 
 	{
 		if(mc.isCanDoAction(msg.sender, _permissionId)){
-			emit GenericCaller_DoActionDirectly(_permissionId);
-
-			// 1 - call immediately?
-			_target.call(
-				bytes4(keccak256(_methodSig)),
-				uint256(32),						 // pointer to the length of the array
-				uint256(_params.length),		 // length of the array
-				_params	
-			);					
-
-			return 0x0;
+			return doActionDirectly(_permissionId, _target, _origin, _methodSig, _params);
 		}else{
-			// 2 - create proposal + voting first  
-			emit GenericCaller_CreateNewProposal(_permissionId);
-
-			// _origin is the initial msg.sender (just like tx.origin) 
-			//GenericProposalTEST prop = new GenericProposalTEST(_target, _origin, _methodSig, _params);
-
-			// TODO: uncomment 
-			//IVoting voting = createVoting(_permissionId, prop, _origin);
-
-			IProposal prop = IProposal(0x0);
-
-			IVoting voting = new Voting_1p1v(mc, prop, _origin, 24 * 60, keccak256("Employees"), bytes32(0));
-			//prop.setVoting(voting);
-
-			// WARNING: should be permitted to add new proposal by the current contract address!!!
-			// check your permissions or see examples (tests) how to do that correctly
-			mc.addNewProposal(prop);		
-			return prop;
+			return doActionWithVoting(_permissionId, _target, _origin, _methodSig, _params);
 		}
+	}
+
+	function doActionDirectly(string _permissionId, address _target, address _origin, 
+									  string _methodSig, bytes32[] _params) internal returns(address proposalOut) {
+		//emit GenericCaller_DoActionDirectly(_permissionId);
+
+		// 1 - call immediately?
+		_target.call(
+			bytes4(keccak256(_methodSig)),
+			uint256(32),						 // pointer to the length of the array
+			uint256(_params.length),		 // length of the array
+			_params	
+		);					
+
+		return 0x0;
+	}
+
+	function doActionWithVoting(string _permissionId, address _target, address _origin, 
+										 string _methodSig, bytes32[] _params) internal returns(address proposalOut) {
+
+		// 2 - create proposal + voting first  
+		//emit GenericCaller_CreateNewProposal(_permissionId);
+
+		GenericProposalTEST prop = new GenericProposalTEST(_target, _origin, _methodSig, _params);
+
+		// TODO: uncomment 
+		//IVoting voting = createVoting(_permissionId, prop, _origin);
+
+		Voting_1p1vTEST voting = new Voting_1p1vTEST(mc, IProposal(0x0), _origin, 
+	   															24 * 60, keccak256("Employees"), bytes32(0));
+		prop.setVoting(voting);
+
+		// WARNING: should be permitted to add new proposal by the current contract address!!!
+		// check your permissions or see examples (tests) how to do that correctly
+		mc.addNewProposal(prop);		
+		return prop;
 	}
 
 	function setVotingParams(string _permissionId, uint _votingType, 
