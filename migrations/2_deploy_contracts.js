80ff78ea88153fc6ce92a5db22fbdefbe03746fb
KIrill Guzenko <kirill@chain.cloud>
2018-06-19 11:06:39 +0300

    trying to fix build

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-var Splitter = artifacts.require("./Splitter") ;
+/*var Splitter = artifacts.require("./Splitter") ;
 var SplitterLib = artifacts.require("./SplitterLib") ;
 
 module.exports = function (deployer) {
 	deployer.deploy(SplitterLib).then(() => {
 		deployer.link(SplitterLib, Splitter);
 	});
-};
+};*/
