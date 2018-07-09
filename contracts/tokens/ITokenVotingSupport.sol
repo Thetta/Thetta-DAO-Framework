968f916192adb23cee916f564b6973c93a502bef
Rostyslav Bortman <bortmanrostyslav@gmail.com>
2018-07-09 19:19:25 +0300

    Fix all warnings #164

--- contracts/tokens/ITokenVotingSupport.sol
+++ contracts/tokens/ITokenVotingSupport.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.22;
 
 interface ITokenVotingSupport {
 	
-	function startNewVoting() public returns(uint);
-	function finishVoting(uint _votingID) public;
-	function getBalanceAtVoting(uint _votingID, address _owner) public view returns (uint256);
+	function startNewVoting() external returns(uint);
+	function finishVoting(uint _votingID) external;
+	function getBalanceAtVoting(uint _votingID, address _owner) external view returns (uint256);
 }
