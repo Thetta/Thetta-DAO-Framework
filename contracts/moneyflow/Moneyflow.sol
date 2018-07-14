e9e1d4107a2c97511980315ae9292d1b5702570c
Rostyslav Bortman <bortmanrostyslav@gmail.com>
2018-07-14 13:42:04 +0300

    refactoring  external -> public

--- contracts/moneyflow/Moneyflow.sol
+++ contracts/moneyflow/Moneyflow.sol
@@ -58,49 +58,41 @@ contract MoneyFlow is IMoneyflow, DaoClient, Ownable {
 
 // IMoneyflow:
 	// will withdraw donations
-	function withdrawDonationsTo(address _out) external isCanDo(WITHDRAW_DONATIONS){
-		_withdrawDonationsTo(_out);
-	}
-
-	function _withdrawDonationsTo(address _out) internal{
+	function withdrawDonationsTo(address _out) public isCanDo(WITHDRAW_DONATIONS){
 		emit MoneyFlow_WithdrawDonations(msg.sender, _out, address(donationEndpoint).balance);
 		donationEndpoint.flushTo(_out);
 	}
 
-	function getDonationEndpoint()external constant returns(IWeiReceiver){
+	function getDonationEndpoint()public constant returns(IWeiReceiver){
 		return donationEndpoint;
 	}
 
-	function getRevenueEndpoint()external constant returns(IWeiReceiver){
+	function getRevenueEndpoint()public constant returns(IWeiReceiver){
 		return rootReceiver;
 	}
 
-	function getDonationEndpointAddress()external constant returns(address){
+	function getDonationEndpointAddress()public constant returns(address){
 		return address(donationF2WR);
 	}
 
-	function getRevenueEndpointAddress()external constant returns(address){
+	function getRevenueEndpointAddress()public constant returns(address){
 		return address(revenueF2WR);
 	}
 
-	function setRootWeiReceiverGeneric(bytes32[] _params) external {
+	function setRootWeiReceiverGeneric(bytes32[] _params) public {
 		IWeiReceiver receiver = IWeiReceiver(address(_params[0]));
-		_setRootWeiReceiver(receiver);
+		setRootWeiReceiver(receiver);
 	}
 
-	function withdrawDonationsToGeneric(bytes32[] _params) external {
+	function withdrawDonationsToGeneric(bytes32[] _params) public {
 		address out = address(_params[0]);
-		_withdrawDonationsTo(out);
+		withdrawDonationsTo(out);
 	}
 
 // WeiReceivers:
 	// receiver can be a splitter, fund or event task
 	// _receiver can be 0x0!
-	function setRootWeiReceiver(IWeiReceiver _receiver) external isCanDo(SET_ROOT_WEI_RECEIVER){
-		_setRootWeiReceiver(_receiver);
-	}
-
-	function _setRootWeiReceiver(IWeiReceiver _receiver) internal{
+	function setRootWeiReceiver(IWeiReceiver _receiver) public isCanDo(SET_ROOT_WEI_RECEIVER){
 		emit MoneyFlow_SetRootWeiReceiver(msg.sender, address(_receiver));
 		rootReceiver = _receiver;
 		revenueF2WR = new FallbackToWeiReceiver(address(rootReceiver));
