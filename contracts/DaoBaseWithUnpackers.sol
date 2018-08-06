4601df16aaca49e48b2faec4b0a90e12e0f943b9
Rostyslav Bortman <bortmanrostyslav@gmail.com>
2018-08-06 12:23:33 +0300

    Fix permission bug #250

--- contracts/DaoBaseWithUnpackers.sol
+++ contracts/DaoBaseWithUnpackers.sol
@@ -11,17 +11,30 @@ import "./DaoBase.sol";
  * After voting is finished -> target method is called and params should be unpacked
 */
 contract DaoBaseWithUnpackers is DaoBase {
+	address autoCaller;
+
 	constructor(DaoStorage _store) public 
 		DaoBase(_store)
 	{
 	}
 
-	function upgradeDaoContractGeneric(bytes32[] _params) external {
+	modifier onlyAutoCaller() { 
+		require (msg.sender == autoCaller); 
+		_; 
+	}
+	
+
+	function setAutoCaller(address _autoCaller) onlyOwner public {
+		autoCaller = _autoCaller;
+	}
+	
+
+	function upgradeDaoContractGeneric(bytes32[] _params) onlyAutoCaller external {
 		IDaoBase _b = IDaoBase(address(_params[0]));
 		upgradeDaoContract(_b);
 	}
 
-	function addGroupMemberGeneric(bytes32[] _params) external {
+	function addGroupMemberGeneric(bytes32[] _params) onlyAutoCaller external {
 		bytes32 group = bytes32(_params[0]);
 		address a = address(_params[1]);
 
@@ -29,7 +42,7 @@ contract DaoBaseWithUnpackers is DaoBase {
 		store.addGroupMember(group, a);
 	}
 
-	function issueTokensGeneric(bytes32[] _params) external {
+	function issueTokensGeneric(bytes32[] _params) onlyAutoCaller external {
 		address _tokenAddress = address(_params[0]);
 		address _to = address(_params[1]);
 		uint _amount = uint(_params[2]);
@@ -37,35 +50,35 @@ contract DaoBaseWithUnpackers is DaoBase {
 		issueTokens(_tokenAddress, _to, _amount);
 	}
 
-	function removeGroupMemberGeneric(bytes32[] _params) external {
+	function removeGroupMemberGeneric(bytes32[] _params) onlyAutoCaller external {
 		string memory _groupName = ConversionLib.bytes32ToString(_params[0]);
 		address _a = address(_params[1]);
 
 		removeGroupMember(_groupName, _a);
 	}
 
-	function allowActionByShareholderGeneric(bytes32[] _params) external {
+	function allowActionByShareholderGeneric(bytes32[] _params) onlyAutoCaller external {
 		bytes32 _what = bytes32(_params[0]);
 		address _a = address(_params[1]);
 
 		allowActionByShareholder(_what, _a);
 	}
 
-	function allowActionByVotingGeneric(bytes32[] _params) external {
+	function allowActionByVotingGeneric(bytes32[] _params) onlyAutoCaller external {
 		bytes32 _what = bytes32(_params[0]);
 		address _tokenAddress = address(_params[1]);
 
 		allowActionByVoting(_what, _tokenAddress);
 	}
 
-	function allowActionByAddressGeneric(bytes32[] _params) external {
+	function allowActionByAddressGeneric(bytes32[] _params) onlyAutoCaller external {
 		bytes32 _what = bytes32(_params[0]);
 		address _a = address(_params[1]);
 
 		allowActionByAddress(_what, _a);
 	}
  
-	function allowActionByAnyMemberOfGroupGeneric(bytes32[] _params) external {
+	function allowActionByAnyMemberOfGroupGeneric(bytes32[] _params) onlyAutoCaller external {
 		bytes32 _what = bytes32(_params[0]);
 		string memory _groupName = ConversionLib.bytes32ToString(_params[1]);
 
