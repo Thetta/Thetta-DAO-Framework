d6c47e0846fcce1fae0af7c86989d66458c3246f
Anthony Akentiev <anthony.akentiev@gmail.com>
2018-08-21 13:19:34 +0300

    Testing linter

--- contracts/tasks/TaskTable.sol
+++ contracts/tasks/TaskTable.sol
@@ -23,7 +23,7 @@ contract TaskTable {
 	enum State {
 		Init,
 		Cancelled,
-		// only for (isPostpaid==false) tasks
+		// only for (isPostpaid==false) Tasks
 		// anyone can use 'processFunds' to send money to this task
 		PrePaid,
 		PostPaid,
@@ -57,30 +57,30 @@ contract TaskTable {
 		uint funds;
 	}
 
-	mapping (uint => Task) tasks;
+	mapping (uint => Task) Tasks;
 
 	modifier onlyWhenStarted(uint _id) { 
-		require (tasks[_id].startTime >= block.timestamp); 
+		require (Tasks[_id].startTime >= block.timestamp); 
 		_; 
 	}
 
 	modifier onlyEmployeeOrMoneySource(uint _id) { 
-		require(msg.sender== tasks[_id].employee || msg.sender==tasks[_id].moneySource); 
+		require(msg.sender== Tasks[_id].employee || msg.sender==Tasks[_id].moneySource); 
 		_; 
 	}
 
 	modifier isCanCancel(uint _id) { 
-		require (block.timestamp - tasks[_id].creationTime <= tasks[_id].timeToCancell); 
+		require (block.timestamp - Tasks[_id].creationTime <= Tasks[_id].timeToCancell); 
 		_; 
 	}
 
 	modifier isDeadlineMissed(uint _id) { 
-		require (block.timestamp - tasks[_id].startTime >= tasks[_id].deadlineTime); 
+		require (block.timestamp - Tasks[_id].startTime >= Tasks[_id].deadlineTime); 
 		_; 
 	}
 
 	modifier onlyByMoneySource(uint _id) {
-		require (tasks[_id].moneySource == msg.sender); 
+		require (Tasks[_id].moneySource == msg.sender); 
 		_; 
 	}
 	
@@ -102,7 +102,7 @@ contract TaskTable {
 		uint64 _deadlineTime, 
 		uint64 _timeToCancell) external returns(uint) 
 	{
-		tasks[elementsCount] = Task(
+		Tasks[elementsCount] = Task(
 			_caption, 
 			_desc, 
 			_isPostpaid, 
@@ -119,16 +119,16 @@ contract TaskTable {
 			0
 		);
 
-		tasks[elementsCount].state = State.Init;
+		Tasks[elementsCount].state = State.Init;
 
-		emit TaskTableElementAdded(elementsCount, tasks[elementsCount].state);
+		emit TaskTableElementAdded(elementsCount, Tasks[elementsCount].state);
 		elementsCount += 1;
 
 		return (elementsCount - 1);
 	}
 
 	function addNewBounty (string _caption, string _desc, uint _neededWei, uint64 _deadlineTime, uint64 _timeToCancell) external returns(uint) {
-		tasks[elementsCount] = Task(
+		Tasks[elementsCount] = Task(
 			_caption, 
 			_desc, 
 			false, 
@@ -144,9 +144,9 @@ contract TaskTable {
 			0
 		);
 
-		tasks[elementsCount].state = State.PrePaid;
+		Tasks[elementsCount].state = State.PrePaid;
 
-		emit TaskTableElementAdded(elementsCount, tasks[elementsCount].state);
+		emit TaskTableElementAdded(elementsCount, Tasks[elementsCount].state);
 		elementsCount += 1;
 
 		return (elementsCount - 1);
@@ -157,45 +157,45 @@ contract TaskTable {
 
 		if(getCurrentState(_id)==State.Init) {
 			// can start only if postpaid task 
-			require(tasks[_id].isPostpaid);
+			require(Tasks[_id].isPostpaid);
 		}
-		tasks[_id].startTime = block.timestamp;
-		tasks[_id].employee = _employee;
-		tasks[_id].state = State.InProgress;
-		emit TaskTableStateChanged(tasks[_id].state);
+		Tasks[_id].startTime = block.timestamp;
+		Tasks[_id].employee = _employee;
+		Tasks[_id].state = State.InProgress;
+		emit TaskTableStateChanged(Tasks[_id].state);
 	}
 
 	// callable by anyone
 	function startBounty(uint _id) public isCanDo(START_BOUNTY) {
 		require(getCurrentState(_id)==State.PrePaid);
-		tasks[_id].startTime = block.timestamp;
-		tasks[_id].employee = msg.sender;
-		tasks[_id].state = State.InProgress;
-		emit TaskTableStateChanged(tasks[_id].state);
+		Tasks[_id].startTime = block.timestamp;
+		Tasks[_id].employee = msg.sender;
+		Tasks[_id].state = State.InProgress;
+		emit TaskTableStateChanged(Tasks[_id].state);
 	}
 
 	// who will complete this task
 	function setEmployee(uint _id, address _employee) onlyByMoneySource(_id) public {
 		emit TaskTableSetEmployee(_employee);
-		tasks[_id].employee = _employee;
+		Tasks[_id].employee = _employee;
 	}
 
 	// where to send money
 	function setOutput(uint _id, address _output) onlyByMoneySource(_id) public {
 		emit TaskTableSetOutput(_output);
-		tasks[_id].output = _output;
+		Tasks[_id].output = _output;
 	}
 
 	function getBalance(uint _id) public view returns(uint) {
-		return tasks[_id].funds;
+		return Tasks[_id].funds;
 	}
 
 	function getCaption(uint _id) public view returns(string) {
-		return tasks[_id].caption;
+		return Tasks[_id].caption;
 	}
 
 	function getDescription(uint _id) public view returns(string) {
-		return tasks[_id].desc;
+		return Tasks[_id].desc;
 	}
 
 	function getCurrentState(uint _id) public view returns(State) {
@@ -211,12 +211,12 @@ contract TaskTable {
 			return State.CanGetFunds;
 		}
 
-		return tasks[_id].state;
+		return Tasks[_id].state;
 	}
 
 	function isTaskPrepaid(uint _id) internal view returns(bool) {
-		if((State.Init==tasks[_id].state) && (tasks[_id].neededWei!=0) && (!tasks[_id].isPostpaid)) {
-			if(tasks[_id].neededWei==tasks[_id].funds && tasks[_id].funds <= address(this).balance) {
+		if((State.Init==Tasks[_id].state) && (Tasks[_id].neededWei!=0) && (!Tasks[_id].isPostpaid)) {
+			if(Tasks[_id].neededWei==Tasks[_id].funds && Tasks[_id].funds <= address(this).balance) {
 				return true;
 			}
 		}
@@ -224,8 +224,8 @@ contract TaskTable {
 	}
 
 	function isTaskPostpaidAndCompleted(uint _id) internal view returns(bool) {
-		if((State.Complete==tasks[_id].state) && (tasks[_id].neededWei!=0) && (tasks[_id].isPostpaid)) {
-			if(tasks[_id].neededWei <= tasks[_id].funds && tasks[_id].funds <= address(this).balance) {
+		if((State.Complete==Tasks[_id].state) && (Tasks[_id].neededWei!=0) && (Tasks[_id].isPostpaid)) {
+			if(Tasks[_id].neededWei <= Tasks[_id].funds && Tasks[_id].funds <= address(this).balance) {
 				return true;
 			}
 		}
@@ -236,63 +236,63 @@ contract TaskTable {
 		require(getCurrentState(_id)==State.Init || getCurrentState(_id)==State.PrePaid);
 		if(getCurrentState(_id)==State.PrePaid) {
 			// return money to 'moneySource'
-			tasks[_id].moneySource.transfer(tasks[_id].funds);
+			Tasks[_id].moneySource.transfer(Tasks[_id].funds);
 		}
-		tasks[_id].state = State.Cancelled;
-		emit TaskTableStateChanged(tasks[_id].state);
+		Tasks[_id].state = State.Cancelled;
+		emit TaskTableStateChanged(Tasks[_id].state);
 	}
 
 	function returnMoney(uint _id) isDeadlineMissed(_id) onlyByMoneySource(_id) public {
 		require(getCurrentState(_id)==State.InProgress);
-		if(address(this).balance >= tasks[_id].funds) {
+		if(address(this).balance >= Tasks[_id].funds) {
 			// return money to 'moneySource'
-			tasks[_id].moneySource.transfer(tasks[_id].funds);
+			Tasks[_id].moneySource.transfer(Tasks[_id].funds);
 		}
-		tasks[_id].state = State.DeadlineMissed;
-		emit TaskTableStateChanged(tasks[_id].state); 
+		Tasks[_id].state = State.DeadlineMissed;
+		emit TaskTableStateChanged(Tasks[_id].state); 
 	}
 
 	function notifyThatCompleted(uint _id) public onlyEmployeeOrMoneySource(_id) {
 		require(getCurrentState(_id)==State.InProgress);
 
-		if((0!=tasks[_id].neededWei) || (tasks[_id].isDonation)) { // if donation or prePaid - no need in ev-ion; if postpaid with unknown payment - neededWei=0 yet
-			tasks[_id].state = State.Complete;
-			emit TaskTableStateChanged(tasks[_id].state);
+		if((0!=Tasks[_id].neededWei) || (Tasks[_id].isDonation)) { // if donation or prePaid - no need in ev-ion; if postpaid with unknown payment - neededWei=0 yet
+			Tasks[_id].state = State.Complete;
+			emit TaskTableStateChanged(Tasks[_id].state);
 		}else {
-			tasks[_id].state = State.CompleteButNeedsEvaluation;
-			emit TaskTableStateChanged(tasks[_id].state);
+			Tasks[_id].state = State.CompleteButNeedsEvaluation;
+			emit TaskTableStateChanged(Tasks[_id].state);
 		}
 	}
 
 	function evaluateAndSetNeededWei(uint _id, uint _neededWei) public onlyByMoneySource(_id) {
 		require(getCurrentState(_id)==State.CompleteButNeedsEvaluation);
-		require(0==tasks[_id].neededWei);
+		require(0==Tasks[_id].neededWei);
 
-		tasks[_id].neededWei = _neededWei;
-		tasks[_id].state = State.Complete;
-		emit TaskTableStateChanged(tasks[_id].state);
+		Tasks[_id].neededWei = _neededWei;
+		Tasks[_id].state = State.Complete;
+		emit TaskTableStateChanged(Tasks[_id].state);
 	}
 
-	// for Prepaid tasks only! 
+	// for Prepaid Tasks only! 
 	// for Postpaid: call processFunds and transfer money instead!
 	function confirmCompletion(uint _id) public onlyByMoneySource(_id) {
 		require(getCurrentState(_id)==State.Complete);
-		require(!tasks[_id].isPostpaid);
-		require(0!=tasks[_id].neededWei);
+		require(!Tasks[_id].isPostpaid);
+		require(0!=Tasks[_id].neededWei);
 
-		tasks[_id].state = State.CanGetFunds;
-		emit TaskTableStateChanged(tasks[_id].state);
+		Tasks[_id].state = State.CanGetFunds;
+		emit TaskTableStateChanged(Tasks[_id].state);
 	}
 
 // IDestination overrides:
 	// pull model
 	function flush(uint _id) public {
 		require(getCurrentState(_id)==State.CanGetFunds);
-		require(0x0!=tasks[_id].output);
+		require(0x0!=Tasks[_id].output);
 
-		tasks[_id].output.transfer(tasks[_id].funds);
-		tasks[_id].state = State.Finished;
-		emit TaskTableStateChanged(tasks[_id].state);
+		Tasks[_id].output.transfer(Tasks[_id].funds);
+		Tasks[_id].state = State.Finished;
+		emit TaskTableStateChanged(Tasks[_id].state);
 	}
 
 	function processFunds(uint _id) public payable {
@@ -300,13 +300,13 @@ contract TaskTable {
 		if(isCanSetNeededWei(_id)) {
 			// this is a donation
 			// client can send any sum!
-			tasks[_id].neededWei = msg.value;
+			Tasks[_id].neededWei = msg.value;
 		}
-		tasks[_id].funds += msg.value;
+		Tasks[_id].funds += msg.value;
 	}
 
 	function isCanSetNeededWei(uint _id) internal view returns(bool) {
-		if(tasks[_id].isPostpaid && (0==tasks[_id].neededWei) && (State.Complete==tasks[_id].state)) {
+		if(Tasks[_id].isPostpaid && (0==Tasks[_id].neededWei) && (State.Complete==Tasks[_id].state)) {
 			return true;
 		}
 
