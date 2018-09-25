691b700927fc913cb94c38c29a8105ba4533e95b
Kirill <kirill@MacBook.local>
2018-09-25 14:14:01 +0700

    migrations improvement

--- scripts/migrateLibs.js
+++ scripts/migrateLibs.js
@@ -1,51 +1,58 @@
-function migrateLibs (artifacts, deployer, network, accounts) {
+function migrateLibs (artifacts, additionalContracts, deployer, network, accounts) {
 
-	async function linkIfExist(lib, contractName){
-		try {
-			await deployer.link(lib, artifacts.require(contractName));
-		} catch(error) {
+     return deployer.then(async () => {      
+          var UtilsLib = artifacts.require("./UtilsLib");
+          var GenericCallerLib = artifacts.require("./GenericCallerLib");
+          var VotingLib = artifacts.require("./VotingLib");
+          var DaoBaseLib = artifacts.require("./DaoBaseLib");
 
-		}
-	}
+          async function linkIfExist(contractName){
+               try {
+                    await deployer.link(UtilsLib, artifacts.require(contractName));
+                    await deployer.link(VotingLib, artifacts.require(contractName));
+                    await deployer.link(GenericCallerLib, artifacts.require(contractName));
+                    await deployer.link(DaoBaseLib, artifacts.require(contractName));
+               } catch(error) {}
+          }
 
-	return deployer.then(async () => {		
-		var UtilsLib = artifacts.require("./UtilsLib");
-		var GenericCallerLib = artifacts.require("./GenericCallerLib");
-		var VotingLib = artifacts.require("./VotingLib");
-		var DaoBaseLib = artifacts.require("./DaoBaseLib");
+          await deployer.deploy(UtilsLib);
+          await deployer.link(UtilsLib, VotingLib);
+          await deployer.deploy(VotingLib);
+          await deployer.link(VotingLib, GenericCallerLib);
+          await deployer.deploy(GenericCallerLib);
+          await deployer.deploy(DaoBaseLib);
 
-		await deployer.deploy(UtilsLib);
-		await deployer.link(UtilsLib, VotingLib);
-		await deployer.deploy(VotingLib);
-		await deployer.link(VotingLib, GenericCallerLib);
-		await deployer.deploy(GenericCallerLib);
-		await deployer.deploy(DaoBaseLib);
+          contractsArr = [ "./DaoBaseImpersonated"
+                         , "./DaoBaseWithUnpackers"
+                         , "./GenericCaller"
+                         , "./VotingLib"
+                         , "./DaoBaseAuto"
+                         , "./DaoBaseWithUnpackersMock"
+                         , "./DaoBaseMock"
+                         , "./DaoStorage"
+                         , "./StdDaoToken"
+                         , "./Voting"
+                         , "./GenericCallerLib"
+                         , "./GenericCaller"
+                         , "./MoneyflowAuto"
+                         , "./DaoBaseAuto"
+                         , "./GenericCaller"
+                         , "./MoneyflowAuto"
+                         , "./DaoBaseAuto"
+                         , "./DaoBase"
+                         , "./DaoBaseWithUnpackers"
+                         , "./DaoBaseWithUnpackersMock"
+                         , "./DaoBaseMock"
+                         ]
 
-		await linkIfExist(UtilsLib, "./DaoBaseImpersonated");
-		await linkIfExist(UtilsLib, "./DaoBaseWithUnpackers");
-		await linkIfExist(UtilsLib, "./GenericCaller");
-		await linkIfExist(UtilsLib, "./VotingLib");
-		await linkIfExist(UtilsLib, "./DaoBaseAuto");
-		await linkIfExist(UtilsLib, "./DaoBaseWithUnpackersMock");
-		await linkIfExist(UtilsLib, "./DaoBaseMock");
-		await linkIfExist(UtilsLib, "./DaoStorage");
-		await linkIfExist(UtilsLib, "./StdDaoToken");
+          contractsArr = contractsArr.concat(additionalContracts);
 
-		await linkIfExist(VotingLib, "./Voting");
-		await linkIfExist(VotingLib, "./GenericCallerLib");
-		await linkIfExist(VotingLib, "./GenericCaller");
-		await linkIfExist(VotingLib, "./MoneyflowAuto");
-		await linkIfExist(VotingLib, "./DaoBaseAuto");
-		
-		await linkIfExist(GenericCallerLib, "./GenericCaller");
-		await linkIfExist(GenericCallerLib, "./MoneyflowAuto");
-		await linkIfExist(GenericCallerLib, "./DaoBaseAuto");
+          for(let contract of contractsArr){
+               await linkIfExist(contract);
+          }
 
-		await linkIfExist(DaoBaseLib, "./DaoBase");
-		await linkIfExist(DaoBaseLib, "./DaoBaseWithUnpackers");
-		await linkIfExist(DaoBaseLib, "./DaoBaseWithUnpackersMock");
-		await linkIfExist(DaoBaseLib, "./DaoBaseMock");
-	});
+          
+     });
 };
 
 module.exports = migrateLibs
