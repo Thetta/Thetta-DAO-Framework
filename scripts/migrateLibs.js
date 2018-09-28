ecd54e367f2f5c55ba9b3d75b2609117cef2f786
Kirill <kirill@MacBook.local>
2018-09-28 17:26:01 +0700

    Migrations reworked

--- scripts/migrateLibs.js
+++ scripts/migrateLibs.js
@@ -1,57 +1,64 @@
+async function linkContractsWithLibrary(deployer, artifacts, lib, contractNames) {
+     for(let contractName of contractNames) {
+          try {
+               await deployer.link(lib, artifacts.require(contractName));
+          } catch(error) {
+               console.log('not linked:', contractName);
+          }
+     }  
+}
+
+let thettaContracts = [ 
+  "./DaoBase",
+  "./DaoBaseAuto",
+  "./DaoBaseImpersonated",
+  "./DaoBaseMock",
+  "./DaoBaseWithUnpackers",
+  "./DaoBaseWithUnpackersMock", 
+  "./DaoStorage",
+  "./GenericCaller",
+  "./MoneyflowAuto",
+  "./StdDaoToken",
+  "./Voting",
+  "./VotingLib",
+  "./GenericCallerLib"
+]
+
 function migrateLibs (artifacts, additionalContracts, deployer, network, accounts) {
-
-     return deployer.then(async () => {      
-          var UtilsLib = artifacts.require("./UtilsLib");
-          var GenericCallerLib = artifacts.require("./GenericCallerLib");
-          var VotingLib = artifacts.require("./VotingLib");
-          var DaoBaseLib = artifacts.require("./DaoBaseLib");
-
-          async function linkIfExist(contractName){
-               try {
-                    await deployer.link(UtilsLib, artifacts.require(contractName));
-                    await deployer.link(VotingLib, artifacts.require(contractName));
-                    await deployer.link(GenericCallerLib, artifacts.require(contractName));
-                    await deployer.link(DaoBaseLib, artifacts.require(contractName));
-               } catch(error) {}
-          }
-
-          await deployer.deploy(UtilsLib);
-          await deployer.link(UtilsLib, VotingLib);
-          await deployer.deploy(VotingLib);
-          await deployer.link(VotingLib, GenericCallerLib);
-          await deployer.deploy(GenericCallerLib);
-          await deployer.deploy(DaoBaseLib);
-
-          contractsArr = [ "./DaoBaseImpersonated"
-                         , "./DaoBaseWithUnpackers"
-                         , "./GenericCaller"
-                         , "./VotingLib"
-                         , "./DaoBaseAuto"
-                         , "./DaoBaseWithUnpackersMock"
-                         , "./DaoBaseMock"
-                         , "./DaoStorage"
-                         , "./StdDaoToken"
-                         , "./Voting"
-                         , "./GenericCallerLib"
-                         , "./GenericCaller"
-                         , "./MoneyflowAuto"
-                         , "./DaoBaseAuto"
-                         , "./GenericCaller"
-                         , "./MoneyflowAuto"
-                         , "./DaoBaseAuto"
-                         , "./DaoBase"
-                         , "./DaoBaseWithUnpackers"
-                         , "./DaoBaseWithUnpackersMock"
-                         , "./DaoBaseMock"
-                         ]
-
-          contractsArr = contractsArr.concat(additionalContracts);
-
-          for(let contract of contractsArr){
-               await linkIfExist(contract);
-          }
-
+     return deployer.then(async () => {    
+          let contractsArr = thettaContracts.concat(additionalContracts);
           
+          try {
+               let UtilsLib = artifacts.require("./UtilsLib");
+               await deployer.deploy(UtilsLib);
+               await linkContractsWithLibrary(deployer, artifacts, UtilsLib, contractsArr);
+          } catch(error) {
+               console.log('UtilsLib does not deploy');
+          }
+
+          try {
+               let VotingLib = artifacts.require("./VotingLib");
+               await deployer.deploy(VotingLib);
+               await linkContractsWithLibrary(deployer, artifacts, VotingLib, contractsArr);
+          } catch(error) {
+               console.log('VotingLib does not deploy');
+          }
+
+          try {
+               let GenericCallerLib = artifacts.require("./GenericCallerLib");
+               await deployer.deploy(GenericCallerLib);
+               await linkContractsWithLibrary(deployer, artifacts, GenericCallerLib, contractsArr);
+          } catch(error) {
+               console.log('GenericCallerLib does not deploy');
+          }
+
+          try {
+               let DaoBaseLib = artifacts.require("./DaoBaseLib");
+               await deployer.deploy(DaoBaseLib);
+               await linkContractsWithLibrary(deployer, artifacts, DaoBaseLib, contractsArr);
+          } catch(error) {
+               console.log('DaoBaseLib does not deploy');
+          }          
      });
 };
 
