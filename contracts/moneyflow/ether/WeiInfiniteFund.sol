842d4a25b9bebdf319f24a761774c4875efa743c
enkogu <kirill@mbook.local>
2018-10-08 21:42:22 +0800

    coments for new funds

--- contracts/moneyflow/ether/WeiInfiniteFund.sol
+++ contracts/moneyflow/ether/WeiInfiniteFund.sol
@@ -1,6 +1,12 @@
 pragma solidity ^0.4.24;
 import "./WeiFund.sol";
 
+/**
+ * @title WeiInfiniteFund 
+ * @dev WeiInfiniteFund is a WeiFund that needs money always
+ * This is a terminal item, that has no children.
+*/
+
 
 contract WeiInfiniteFund is WeiFund {
 	constructor() public WeiFund(0, false, false, 0) {
--- contracts/moneyflow/ether/WeiOneTimeFund.sol
+++ contracts/moneyflow/ether/WeiOneTimeFund.sol
@@ -1,6 +1,13 @@
 pragma solidity ^0.4.24;
 import "./WeiFund.sol";
 
+/**
+ * @title WeiInfiniteFund 
+ * @dev WeiInfiniteFund is a WeiFund that needs money util collect _neededWei
+ * This is a terminal item, that has no children.
+*/
+
+
 
 contract WeiOneTimeFund is WeiFund {
 	constructor(uint _neededWei) public WeiFund(_neededWei, false, false, 0) {
