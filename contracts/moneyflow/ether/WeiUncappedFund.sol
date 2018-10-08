191cf26a57606c7d23be0564c023db720da3ccca
enkogu <kirill@mbook.local>
2018-10-08 21:43:59 +0800

    WeiUncappedFund

--- contracts/moneyflow/ether/WeiUncappedFund.sol
+++ contracts/moneyflow/ether/WeiUncappedFund.sol
@@ -2,14 +2,14 @@ pragma solidity ^0.4.24;
 import "./WeiFund.sol";
 
 /**
- * @title WeiInfiniteFund 
- * @dev WeiInfiniteFund is a WeiFund that needs money util collect _neededWei
+ * @title WeiUncappedFund 
+ * @dev WeiUncappedFund is a WeiFund that will keep receiving ANY amount of ETH forever
  * This is a terminal item, that has no children.
 */
 
 
 
-contract WeiOneTimeFund is WeiFund {
+contract WeiUncappedFund is WeiFund {
 	constructor(uint _neededWei) public WeiFund(_neededWei, false, false, 0) {
 	}
 }
